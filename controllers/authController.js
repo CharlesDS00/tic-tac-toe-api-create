406041ad6c03acce90744d47701afa80902f9fc3
Charles de Souza <desouzacharles5@gmail.com>
2024-09-14 13:57:38 +0200

    controllers auth

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.register = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    let user = await User.findOne({ username });
+    if (user) return res.status(400).json({ msg: 'User already exists' });
+
+    user = new User({ username, password });
+    await user.save();
+
+    const token = jwt.sign({ id: user.id }, 'secretKey', { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user.id }, 'secretKey', { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
