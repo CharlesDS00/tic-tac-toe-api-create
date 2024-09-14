c742f225371a1b29622b4e16bcff6ee670b8801a
Charles de Souza <desouzacharles5@gmail.com>
2024-09-14 15:21:47 +0200

    express mongoose

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports.connect = () => {
-  mongoose.connect('mongodb://localhost:27017/tictactoe', { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect('mongodb://localhost:27017/tictactoe')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 };
